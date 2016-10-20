b64be7e7caa090753848a8f27650ce809a0d80ef
Gonzalo Diethelm <gonzalo.diethelm@booking.com>
2016-10-20 18:15:59 +0200

    Add some comments

--- tryit.pl
+++ tryit.pl
@@ -3,10 +3,13 @@ use warnings;
 use NGHTTP2::Session;
 
 my $session = NGHTTP2::Session->new({
-    on_send => sub { print "heya\n" },
-    on_recv => sub { print "again\n" },
+    on_send => sub { print "inside Perl on_send\n" },
+    on_recv => sub { print "inside Perl on_recv\n" },
 });
 $session->open_session();
 
+# This one prints its message once
 $session->recv();
-$session->send();
+
+# This one gets into a loop printing its message
+# $session->send();
