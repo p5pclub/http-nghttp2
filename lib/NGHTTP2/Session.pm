48e3c04eae149860059efdadf772e5dfc4d2354f
Sawyer X <xsawyerx@cpan.org>
2016-10-20 17:11:50 +0200

    add session

---
+++ lib/NGHTTP2/Session.pm
@@ -0,0 +1,39 @@
+package NGHttp2::Session;
+
+use strict;
+use warnings;
+
+use XSLoader;
+
+our $VERSION = '0.000001';
+XSLoader::load(__PACKAGE__, $VERSION);
+
+1;
+
+__END__
+
+=pod
+
+=encoding utf8
+
+=head1 NAME
+
+HTTP::NGHttp2::Session - Perl binding for nghttp2 library
+
+=head1 VERSION
+
+Version 0.000001
+
+=head1 SYNOPSIS
+
+=head1 DESCRIPTION
+
+=head1 AUTHORS
+
+=over 4
+
+=item * Gonzalo Diethelm C<< gonzus AT cpan DOT org >>
+
+=item * Sawyer X C<< xsawyerx AT cpan DOT org >>
+
+=back
