1e4e129491b23f01e30033d0a94d52013dc91968
Sawyer X <xsawyerx@cpan.org>
2016-11-10 17:27:15 +0100

    add the request class

---
+++ lib/NGHTTP2/Request.pm
@@ -0,0 +1,33 @@
+package NGHTTP2::Request;
+
+use Moo;
+use MooX::Types::MooseLike::Base qw< Str Int >;
+
+has [qw<method scheme authority path>] => (
+    'is'       => 'ro',
+    'isa'      => Str,
+    'required' => 1,
+);
+
+has 'user_agent' => (
+    'is'      => 'ro',
+    'isa'     => Str,
+    'default' => sub {
+        my $version = $NGHTTP2::Client::VERSION || 'dev';
+        return "perl/NGHTTP2::Client/$version";
+    },
+);
+
+sub finalize {
+    my $self = shift;
+
+    return [
+        [ ':method'    => $self->method ],
+        [ ':scheme'    => $self->scheme ],
+        [ ':authority' => $self->authority ],
+        [ ':path'      => $self->path ],
+        [ 'user-agent' => $self->user_agent ],
+    ];
+}
+
+1;
